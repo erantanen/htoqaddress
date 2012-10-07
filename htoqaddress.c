a7ee64a215afd06c4d82cf105da0abd9e9d84855
erantanen <rantanen_ed@yahoo.com>
2012-10-07 15:47:17 -0400

    update

--- htoqaddress.c
+++ htoqaddress.c
@@ -13,14 +13,42 @@ void main()
 
 int i;
 
-char c[4]  = {0x0A,0x00,0x02,0x0F};
+//char c[4]  = {0x0A,0x00,0x02,0x0F};
+unsigned char c[4]  = {0x0a,0x00,0x02,0xff};
 
-int i_0 = (int) c[0];
-int i_1 = (int) c[1];
-int i_2 = (int) c[2];
-int i_3 = (int) c[3];
 
-   printf("%d.%d.%d.%d", (int)c[0], (int)c[1], (int)c[2], (int)c[3]);
+char c_char;
 
+char q_address[16];
+
+
+
+printf("-----------------------\n");
+
+  for(i = 0; i < sizeof(c); i++)
+   {
+     int c_int  = (int) c[i];
+
+      if(c_int < 10 ) {
+           c_char = (char) c_int;
+       }else{
+     
+         c_int = c_int % 10;
+          c_char = (char) c_int;  
+       }   
+
+      printf("%d %d  %c \n", i, (int)c[i], (c_char + 48) );
+    }
+
+printf("-----------------------\n\n");
+
+
+   sprintf(q_address, "%d.%d.%d.%d", (int)c[0], (int)c[1], (int)c[2], (int)c[3]);
+   printf("decimals have been concated into a string\n");
+
+   printf("%s -> as a string\n", q_address);
+        
+   printf("%d.%d.%d.%d -> as . seperated decimals", (int)c[0], (int)c[1], (int)c[2], (int)c[3]);
    printf("\n\n");
+
 }
